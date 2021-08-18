6ec1494fea74ff0deb2949c50b27242eb4feb21a
drumboy-addiction <abhijeetkujur000@gmail.com>
2021-08-18 16:45:12 +0530

    Created the assignment

---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+const Footer = () => {
+    return (
+        <div className="foots">
+            <h1 className="footing">This is a Footer component</h1>
+            <p className="subs">Component 2</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './header.css'
+const Header = () => {
+    return (
+        <div className="heads">
+            <h1 className="heading">This is a Header component</h1>
+            <p className="sub">Component 1</p>
+        </div>
+    )
+}
+
+export default Header
